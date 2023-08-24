df41c772a59fa37a7c8ee442cf6ee8df261f5c1b
Julio <killer.of.time@protonmail.com>
2023-08-24 17:35:38 +1000

    sign up now using testSignIn flow

--- modules/TideJS/Functions/SignUp.js
+++ modules/TideJS/Functions/SignUp.js
@@ -23,6 +23,7 @@ import dKeyAuthenticationFlow from "../Flow/dKeyAuthenticationFlow.js"
 import TideJWT from "../ModelsToSign/TideJWT.js"
 import dDecryptionTestFlow from "../Flow/dDecryptionTestFlow.js"
 import HashToPoint from "../Tools/H2P.js"
+import TestSignIn from "./TestSignIn.js"
 
 export default class SignUp {
     /**
@@ -131,8 +132,9 @@ export default class SignUp {
         if(modelToSign == null && this.modelToSign == null) this.mode = "default"; // revert mode to default if no model to sign provided
 
         // Test sign in
-        const {jwt, modelSig} = await this.testSignIn(this.savedState.uid, this.savedState.gUser, this.savedState.gPass, this.savedState.gVVK, this.savedState.cmkPub, this.savedState.cvkPub, modelToSign);
-
+        const testSignIn = new TestSignIn(this.cmkOrkInfo, this.cvkOrkInfo, true);
+        const {jwt, modelSig} = await testSignIn.start(this.savedState.uid, this.savedState.gUser, this.savedState.gPass, this.savedState.gVVK, this.savedState.cmkPub, this.savedState.cvkPub, modelToSign)
+        
         // Test dDecrypt
         if(this.mode == "default"){
             // implement flag for which tests we want to run in new account
@@ -153,39 +155,6 @@ export default class SignUp {
             TideJWT: jwt, 
             modelSig: modelSig
         };
-    }
-
-    /**
-     * @param {string} uid
-     * @param {Point} gUser 
-     * @param {Point} gPass 
-     * @param {string} gVVK 
-     * @param {Point} cmkPub 
-     * @param {Point} cvkPub 
-     * @param {string} modelToSign
-     * @returns 
-     */
-    async testSignIn(uid, gUser, gPass, gVVK, cmkPub, cvkPub, modelToSign_p=null){
-        const modelRequested = (this.modelToSign == null && modelToSign_p == null) ? false : true;
-        const modelToSign = this.modelToSign == null ? modelToSign_p : this.modelToSign; // figure out which one is the not null, if both are null, it will still be null
-
-        const startTime = BigInt(Math.floor(Date.now() / 1000));
-        const r1 = RandomBigInt();
-        const r2 = RandomBigInt();
-
-        const gBlurUser = gUser.times(r2);
-        
-        const gBlurPass = gPass.times(r1);
-
-        const authFlow = new dKeyAuthenticationFlow(this.cmkOrkInfo, false, false);
-        const convertData = await authFlow.Convert(uid, gBlurUser, gBlurPass, r1, r2, startTime, cmkPub, gVVK);
-        
-        authFlow.CVKorks = this.cvkOrkInfo;
-        const authData = await authFlow.Authenticate_and_PreSignInCVK(uid, convertData.VUID, convertData.decChallengei, convertData.encAuthRequests, convertData.gSessKeyPub, convertData.data_for_PreSignInCVK, modelRequested);
-
-        const resp = await authFlow.SignInCVK(convertData.VUID, convertData.jwt, authData.vlis, convertData.timestamp2, convertData.data_for_PreSignInCVK.gRMul, authData.gCVKR, authData.S, authData.ECDHi, authData.gBlindH, this.mode, modelToSign, authData.model_gR);
-        if(!(await TideJWT.verify(resp.jwt, cvkPub))) throw Error("Test sign in failed");
-        return resp;
     }
 
     /**
